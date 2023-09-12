dc57be85f4f79f6a7c21ec984967d5e1b2ae1831
Gustavo Buziki <gustavo.buziki@paipe.co>
2023-09-12 20:53:19 -0300

    feat: application screen

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+import { Button } from "./button";
+import { Label } from "./label"; 
+import { Select } from "./select";
+import { Separator } from "./separator";
+import { Slider } from "./slider";
+import { Textarea } from "./textarea";
+
+export { Button, Label, Select, Separator, Slider, Textarea }
